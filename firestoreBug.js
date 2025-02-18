b88e5efebdb824ae5b2389a2fe71df9f78dbba13
BugHunter-X <askzenai@gmail.com>
2025-02-18 16:56:05 +0100

    Add firestoreBug.js file

---
+++ firestoreBug.js
@@ -0,0 +1,1 @@
+In a Firebase project, I encountered an issue where data wasn't being written to the Firestore database despite seemingly correct code. The `set()` method appeared to execute without errors, but the data was missing from the database.  The issue only occurred intermittently. 
